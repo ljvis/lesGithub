03ae7a95af1bfe2d891caeb9ebad0d9121185dee
LJ Vis <ljvis@me.com>
2015-07-30 11:16:33 +0200

    Tweede commentaar
    bla bla

---
+++ GithubLes/GithubLes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubLes
+//
+//  Created by ljvis42 on 30-07-15.
+//  Copyright (c) 2015 ljvis42. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
